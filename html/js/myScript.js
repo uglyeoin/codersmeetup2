65de2729525cc496daf74aae8086e3b2b0b973eb
Eoin Oliver <eoin@squareballoon.co.uk>
2020-01-26 13:55:38 +0000

    added json

--- html/js/myScript.js
+++ html/js/myScript.js
@@ -1,18 +1,40 @@
-var url = "https://codersuk-test.s3-eu-west-1.amazonaws.com/data.json";
+// JSON
+var jsonURL     = "https://codersuk-test.s3-eu-west-1.amazonaws.com/data.json";
+var jsonArray   = window.fetch(jsonURL);
 
+// API Amazon Lambda
+var apiURL      = "https://2tf15xu7pg.execute-api.us-east-1.amazonaws.com/default/eoinsLambdaFunction";
+var apiKey      = "x-api-key";
+var apiValue    = "oEaZASeL3V30AevqVdDjT9VenBCi3O5Zsy7kqQ14";
+var fullApiKey  = apiURL + apiKey + apiValue;
 
-fetch(url, {method: 'get'}) // Call the fetch function passing the url of the API as a parameter
+fetch(jsonURL, {method: 'get'}) // Call the fetch function passing the jsonURL of the API as a parameter
 .then(function() {
     // Your code for handling the data you get from the API
     console.log ("200: Success.  ES6 Method");
-    console.log (url);
-    console.log(typeof(url));
 })
 // Retrieve its body as ReadableStream
-window.fetch(url)
+window.fetch(jsonURL)
     .then(function(response){
         return response.json();
     }).then(function(json){
         console.log(json);
         console.log(json[0].company);
-    });
+    }).then(function(json){
+        for (i = 0; i < json[0].length; i++) {
+            x += json[0][i];
+            console.log(x);
+        } 
+    })
+    ;
+
+
+
+    // API Stuff
+window.fetch(fullApiKey)
+    .then(function(response){
+        return response.json();
+    }).then(function(json){
+        console.log(json);
+        console.log(json[0].body);
+    });    
